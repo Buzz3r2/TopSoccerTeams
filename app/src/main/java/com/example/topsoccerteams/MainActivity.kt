c2e967f07b189895fcd302474ff0d6615563c5fe
Buzz3r2 <saadiqsteyn1@gmail.com>
2026-04-08 08:52:12 +0200

    Added new background image

--- app/src/main/java/com/example/topsoccerteams/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteams/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val teams = arrayOf<String>("Mamelodi Sundowns FC", "Orlando Pirates",
+        "Bidvest Wits", "Stellenbosch"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
